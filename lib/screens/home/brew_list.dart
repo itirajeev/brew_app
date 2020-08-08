87329b312eb64f783fe27e9e8a9b1fb2542a4e59
itirajeev <iti@oneseeker.com>
2020-08-08 16:24:01 +0530

    final settings

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -12,11 +12,6 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context) ?? [];
-//    brews.forEach((brew) {
-//      print(brew.name);
-//      print(brew.sugars);
-//      print(brew.strength);
-//    });
 
     return ListView.builder(
       itemCount: brews.length,
